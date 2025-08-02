8e89a04cf8da819186214bd54869154f88790f72
Logan Sullivan <logan.q.sullivan@gmail.com>
2025-08-02 18:49:07 -0500

    added lookup index of letters in alphabet

--- .vscode/main.cpp
+++ .vscode/main.cpp
@@ -5,13 +5,27 @@
 using namespace std;
 
 int main(void) {
-    cout << "word to decipher" << endl;
-
+    // create dictionary to relate letter to number
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+    // take input of word
     string word_in;
+    cout << "word to decipher" << endl;
     cin >> word_in;
 
+    // generate indices of letters in word
+    int word_indices[word_in.length()];
+    for (int i = 0; i < word_in.length(); i++) {
+        word_indices[i] = alphabet.find(word_in[i]) + 1;
+        cout << i << ": " << word_indices[i] << endl;
+    }
+
+    // print word, length, and indices
     cout << "you entered " << word_in << endl;
     cout << "it\'s " << word_in.length() << " characters" << endl;
+    // for (int ele : word_indices) {
+    //     cout << ele << " ";
+    // }
 
     return 0;
 }
