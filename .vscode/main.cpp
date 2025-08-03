9c0128da0552f857867632473abe9dab12ef7eb4
Logan Sullivan <logan.q.sullivan@gmail.com>
2025-08-03 14:15:43 -0500

    added core cipher on vectors and started permutation

--- .vscode/main.cpp
+++ .vscode/main.cpp
@@ -4,6 +4,11 @@
 
 using namespace std; 
 
+// TODO: put on github, permutations of operators, command line or batch mode
+// https://code.visualstudio.com/docs/sourcecontrol/intro-to-git 
+// https://www.geeksforgeeks.org/dsa/print-all-possible-permutations-of-an-array-vector-without-duplicates-using-backtracking/
+// https://stackoverflow.com/questions/11315854/input-from-command-line
+
 int do_operation(int a, int b, int op_index) {
     int out;
 
@@ -20,6 +25,19 @@ int do_operation(int a, int b, int op_index) {
     // TODO: error handling for bad operator index?
 
     return out;
+}
+
+int do_core_cipher(vector<int> word_values, vector<int> operation_seq) {
+    if (word_values.size() + 1 < operation_seq.size()) {
+        throw logic_error("Not enough operations for word length");
+    }
+
+    int c = word_values[0];
+    for (int i = 1; i < word_values.size(); i ++) {
+        c = do_operation(c, word_values[i], operation_seq[i - 1]);
+    }
+
+    return c;
 }
 
 int main(void) {
@@ -42,7 +60,10 @@ int main(void) {
     // put one of +,-,*,/ between the numbers, no repeats
     // find the smallest positive integer that results
     int core_cipher = ((word_indices[0] + word_indices[1]) - word_indices[2]) * word_indices[3];
-
+    char core_cipher_letter;
+    if (core_cipher < 26) {
+        core_cipher_letter = alphabet[core_cipher];
+    }
     // print word, length, and indices
     cout << "word       : " << word_in << endl;
     cout << "indices    : ";
@@ -50,9 +71,28 @@ int main(void) {
         cout << ele << " ";
     }
     cout << endl;
-    cout << "core cipher: " << core_cipher << endl;
+    cout << "core cipher: " << core_cipher + 1 << endl;
+    cout << "core letter: " << core_cipher_letter << endl;
 
     cout << "test operations: " << do_operation(3, 4, 1) << " " << do_operation(3, 4, 2) << " " << do_operation(3, 4, 3) << " " << do_operation(3, 4, 4) << endl;
+
+    cout << "test core cipher: " << do_core_cipher({5, 2, 3}, {3, 2}) << endl;
+    // treat each operation as an integer, make permutations of those integers
+    // then index into the operations
+    string perm = "1234";
+    do 
+    {
+        for (char i : perm) {
+            cout << i;
+        }
+        cout << "\n";
+        // store permutation as vector
+        vector<int> perm_int(perm.length());
+        for (int i = 0; i < perm.length(); i++) {
+            perm_int[i] = perm[i] - '0';
+        }
+    }
+    while (std::next_permutation(perm.begin(), perm.end()));
 
     return 0;
 }
---
+++ easy_version.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+
+find_letter <- function(word, verbose = FALSE) {
+  word_index <- rep(NA, nchar(word))
+  for (i in 1:nchar(word)) {
+    word_index[i] <- which(letters == substr(word, i, i))
+  }
+  
+  candidates <- 
+    expand_grid(
+      a = c("+", "-", "*", "/"),
+      b = c("+", "-", "*", "/"),
+      c = c("+", "-", "*", "/")
+    ) %>%
+    filter(a != b,
+           a != c,
+           b != c) %>%
+    mutate(expr = paste("((", word_index[1], a, word_index[2], ")", b, 
+                        word_index[3], ")", c, word_index[4])) %>%
+    rowwise() %>%
+    mutate(res  = eval(parse(text = expr))) %>%
+    ungroup() %>%
+    filter(res == (res - (res %% 1)),
+           res > 0,
+           res < 27) %>%
+    arrange(res)
+  
+  if (verbose) {
+    return(candidates)
+  } else {
+    return(candidates %>%
+             `[`(1, ) %>% 
+             pull(res)
+           # %>%
+           #   {`[`(letters, .)}
+    )
+  }
+}
+
+find_letter("maid", verbose = TRUE)
+
+c("pigs", "sand", "mail", "date", "head", 
+  "clam", "peak", "heat", "joya", "well",
+  "toad", "card", "will", "tape", "legs", 
+  "tree", "road", "maid", "slab", "rock",
+  "hand", "vase", "safe", "clay", "toes") %>%
+  vapply(X = .,
+         FUN = find_letter,
+         FUN.VALUE = double(1)) %>%
+  paste(collapse = " ")
+
