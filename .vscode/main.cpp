8678efb9f8f960c7a4748d2359797ef36b3dcb0b
Logan Sullivan <logan.q.sullivan@gmail.com>
2025-08-02 19:07:30 -0500

    added core cipher with fixed operators

--- .vscode/main.cpp
+++ .vscode/main.cpp
@@ -2,13 +2,14 @@
 #include <string>
 #include <bits/stdc++.h>
 
-using namespace std;
+using namespace std; 
 
 int main(void) {
     // create dictionary to relate letter to number
     string alphabet = "abcdefghijklmnopqrstuvwxyz";
     
     // take input of word
+    // TODO: max 5 letters
     string word_in;
     cout << "word to decipher" << endl;
     cin >> word_in;
@@ -17,15 +18,21 @@ int main(void) {
     int word_indices[word_in.length()];
     for (int i = 0; i < word_in.length(); i++) {
         word_indices[i] = alphabet.find(word_in[i]) + 1;
-        cout << i << ": " << word_indices[i] << endl;
     }
 
+    // compute core ciphers
+    // put one of +,-,*,/ between the numbers, no repeats
+    // find the smallest positive integer that results
+    int core_cipher = ((word_indices[0] + word_indices[1]) - word_indices[2]) * word_indices[3];
+
     // print word, length, and indices
-    cout << "you entered " << word_in << endl;
-    cout << "it\'s " << word_in.length() << " characters" << endl;
-    // for (int ele : word_indices) {
-    //     cout << ele << " ";
-    // }
+    cout << "word       : " << word_in << endl;
+    cout << "indices    : ";
+    for (int ele : word_indices) {
+        cout << ele << " ";
+    }
+    cout << endl;
+    cout << "core cipher: " << core_cipher << endl;
 
     return 0;
 }
